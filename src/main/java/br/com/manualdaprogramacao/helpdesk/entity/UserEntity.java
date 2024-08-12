4e8a3fac46c976768f62e40455b1ca5354feca7b
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-11 21:52:54 -0300

    Criação UserEntity

---
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/entity/UserEntity.java
@@ -0,0 +1,41 @@
+package br.com.manualdaprogramacao.helpdesk.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String email;
+
+    private boolean active;
+
+    @Column(name = "created_by")
+    private UUID createdBy;
+
+    @Column(name = "created_at")
+    private UUID createAt;
+
+    @Column(name = "updated_by")
+    private UUID updatedBy;
+
+    @Column(name = "updated_at")
+    private UUID updatedAt;
+}
