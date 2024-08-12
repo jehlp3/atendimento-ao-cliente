6aa9966c5464e8d72a9fad8d2618b694346af9d8
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-11 21:53:27 -0300

    Criação comando SQL para criação do usuário

---
+++ src/main/resources/db/migration/V1_1_create_users_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id uuid primary key,
+    username varchar(40) not null unique,
+    password varchar(150) not null,
+    name varchar(150) not null,
+    email varchar(200) not null unique,
+    active boolean not null,
+    created_at timestamp not null,
+    created_by uuid null,
+    updated_at timestamp not null,
+    updated_by uuid null, --padrão sql nome_aaa
+)
