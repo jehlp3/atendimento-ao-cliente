d24413b0ad9d34a8c9c3797a9a1284991d720f09
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-11 21:53:04 -0300

    Criação UserRepository

---
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.manualdaprogramacao.helpdesk.repository;
+
+import br.com.manualdaprogramacao.helpdesk.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+
+}
