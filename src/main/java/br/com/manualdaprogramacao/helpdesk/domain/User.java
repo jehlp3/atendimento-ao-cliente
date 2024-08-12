52977701c1769ae77a5b171e44b75013eac8a016
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-12 16:05:42 -0300

    Criação User Domain

---
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/domain/User.java
@@ -0,0 +1,13 @@
+package br.com.manualdaprogramacao.helpdesk.domain;
+
+import java.util.UUID;
+
+public record User(
+        UUID id,
+        String username,
+        String password,
+        String name,
+        String email
+) {
+
+}
