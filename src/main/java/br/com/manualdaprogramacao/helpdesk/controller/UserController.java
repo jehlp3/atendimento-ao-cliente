8620432e4fa43dd4115fa9e3438bcdbfb80cc826
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-12 16:06:26 -0300

    Criação User Controller

--- src/main/java/br/com/manualdaprogramacao/helpdesk/controller/UserController.java
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/controller/UserController.java
@@ -1,16 +1,23 @@
 package br.com.manualdaprogramacao.helpdesk.controller;
 
+import br.com.manualdaprogramacao.helpdesk.dto.CreateUserDto;
+import br.com.manualdaprogramacao.helpdesk.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping(path = "/users")
 public class UserController {
 
+    private final UserService userService;
+
     @PostMapping
-    public ResponseEntity create(){
+    public ResponseEntity create(@RequestBody CreateUserDto request){
         System.out.println("Usuário criado");
         return ResponseEntity.noContent().build();
     }
