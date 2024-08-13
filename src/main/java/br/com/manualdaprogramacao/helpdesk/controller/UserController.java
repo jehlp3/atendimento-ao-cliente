c6d5a693479fce0fce295d934b8774c2eaddfb56
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-13 12:02:29 -0300

    Atualizando requisição de usuário

--- src/main/java/br/com/manualdaprogramacao/helpdesk/controller/UserController.java
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/controller/UserController.java
@@ -1,5 +1,6 @@
 package br.com.manualdaprogramacao.helpdesk.controller;
 
+import br.com.manualdaprogramacao.helpdesk.domain.User;
 import br.com.manualdaprogramacao.helpdesk.dto.CreateUserDto;
 import br.com.manualdaprogramacao.helpdesk.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,7 +21,14 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity create(@RequestBody CreateUserDto request){
-        System.out.println("Usuário criado");
+        User domain = new User(
+                UUID.randomUUID(),
+                request.username(),
+                request.password(),
+                request.name(),
+                request.email()
+                );
+        userService.createUser(domain);
         return ResponseEntity.noContent().build();
     }
 }
