2d131cd7628748879a12873f01ad61d2dbea9f79
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-19 10:39:52 -0300

    Create Ticket Dto

---
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/dto/TicketDto.java
@@ -0,0 +1,30 @@
+package br.com.manualdaprogramacao.helpdesk.dto;
+
+import br.com.manualdaprogramacao.helpdesk.entity.UserEntity;
+import br.com.manualdaprogramacao.helpdesk.enums.TicketStatus;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+public class TicketDto {
+
+    private UUID id;
+
+    private UserDto supportUser;
+
+    private String subject;
+
+    private String description;
+
+    private TicketStatus status;
+
+    private UserEntity createdBy;
+
+    private Date createAt;
+
+    private UUID updatedBy;
+
+    private Date updatedAt;
+}
