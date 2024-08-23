b41516bb1c9f0b621117604bf31ce0e834b20e12
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-23 17:02:55 -0300

    Addition of GET method for listing Tickets.

--- src/main/java/br/com/manualdaprogramacao/helpdesk/controller/TicketController.java
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/controller/TicketController.java
@@ -13,11 +13,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @OpenAPIDefinition
@@ -45,5 +43,12 @@ public class TicketController {
         domain.setTicketId(ticketId);
         TicketDto updatedTicket = mapper.toDto(ticketService.ticketInteract(domain));
         return ResponseEntity.ok(updatedTicket);
+    }
+
+    @Operation(description = "This method returns the ticket list")
+    @GetMapping
+    public ResponseEntity<List<TicketDto>> listAllTickets(){
+        List<TicketDto> tickets = mapper.toDto(ticketService.listAll());
+        return ResponseEntity.ok(tickets);
     }
 }
