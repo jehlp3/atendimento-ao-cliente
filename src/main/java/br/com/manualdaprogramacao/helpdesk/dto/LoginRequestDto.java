0037d03dfd6afeb1c5eb2a9946dd30502cc293e0
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-27 17:34:52 -0300

    Create Login Request Dto

---
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/dto/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package br.com.manualdaprogramacao.helpdesk.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginRequestDto {
+
+    String username;
+    String password;
+
+}
