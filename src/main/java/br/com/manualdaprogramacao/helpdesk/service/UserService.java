a71122224d15a3efbf77dd0cd660540159b2d6e7
jehlp3 <jessicalacerdap@hotmail.com>
2024-08-27 17:37:43 -0300

    Update encrypted passwords in the database

--- src/main/java/br/com/manualdaprogramacao/helpdesk/service/UserService.java
+++ src/main/java/br/com/manualdaprogramacao/helpdesk/service/UserService.java
@@ -6,6 +6,7 @@ import br.com.manualdaprogramacao.helpdesk.mapper.UserMapper;
 import br.com.manualdaprogramacao.helpdesk.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -30,6 +31,7 @@ public class UserService {
 
         UserEntity entity = mapper.toEntity(newUser);
         entity.setCreateAt(new Date());
+        entity.setPassword(new BCryptPasswordEncoder().encode(entity.getPassword())); //Encriptografar a senha ao salvar no DB
         entity = userRepository.save(entity);
         return mapper.toDomain(entity);
     }
